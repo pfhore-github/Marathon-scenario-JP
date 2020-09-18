03b9c5979f19e968486b97df9b49a2ef224f0726
pfhore-github <ookawa.michiaki@gmail.com>
2020-09-18 21:41:50 +0900

    Phoenix翻訳を追加
    Phoenix翻訳を追加

---
+++ phoenix/37 Wetworks/gimmicks 02.lua
@@ -0,0 +1,66 @@
+regen_ticker = 0
+
+Triggers = {}
+
+function Triggers.cleanup()
+   for p in Players() do
+      p.items["alien weapon ammo"] = 0
+   end
+end
+
+function Triggers.idle()
+   for p in Players() do
+      if p.z < -12 and not p.dead then
+         p:damage(15,"suffocation")
+      end
+   end
+   for m in Monsters() do
+      if m.z < -12 and not m.dead and not m.player then
+         m:damage(45,"suffocation")
+      end
+      if m.z < -2.00 and not m.player then
+         m.external_velocity = 0
+      end
+   end
+   for p in Players() do
+      if p.extravision_duration > 0 then
+         if regen_ticker == 0 and p.life < 150 and not p.dead then
+            p.life = p.life + 1
+         end
+         if regen_ticker == 1 and p.life < 150 and not p.dead then
+            p.life = p.life + 1
+         end
+         if regen_ticker == 2 and p.life < 300 and not p.dead then
+            p.life = p.life + 1
+         end
+         if regen_ticker == 3 and p.life < 450 and not p.dead then
+            p.life = p.life + 1
+         end
+      end
+      if p.invincibility_duration == 4998 then
+         p.invincibility_duration = 0
+      end
+      if p.action_flags.right_trigger and not p.dead then
+         if p.weapons.current.type == "alien weapon" and p.weapons["alien weapon"].secondary.rounds > 0 then
+            p.invincibility_duration = 5000
+         end
+      end
+   end
+   regen_ticker = regen_ticker + 1
+   if regen_ticker > 3 then
+      regen_ticker = 0
+   end
+end
+
+function Triggers.got_item(item,player)
+   if item.mnemonic == "alien weapon ammo" then
+      for p in Players() do
+         p.items["extravision"] = p.items["extravision"] + 1
+      end
+   end
+   if item == "infravision" then
+      player.infravision_duration = 0
+      player.items["infravision"] = 0
+      player.extravision_duration = 1350
+   end
+end
